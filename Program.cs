26a6cfed251bfb220d6e860b5575b6130700731f
Kamila <89898648749@mail.ru>
2023-12-20 20:21:17 +0300

    zadanie 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿// Задача 2: Задайте двумерный массив. Напишите программу, 
-//которая поменяет местами первую и последнюю строку массива.
+﻿// Задача 3: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+using System.ComponentModel.DataAnnotations;
 
 int [,] array = new int[5, 4];
 
@@ -28,21 +30,27 @@ void PrintArray()
     Console.WriteLine();
 }
 
-
-void PrintNumbers()
+void PrintMinSum()
 {
-    int newNum;
-    int i = 0;
-    for (int j = 0; j < array.GetLength(1); j++)
+    int min = int.MaxValue;
+    int index = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        newNum = array[i,j];
-        array[i,j] = array[array.GetLength(0) - 1, j];
-        array[array.GetLength(0) - 1, j] = newNum;
+        int summa = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summa += array[i, j];
+        }
+        if(summa < min)
+        {
+            index = i;
+            min = summa;
+        }
     }
+    Console.Write($"min stroka {index}");
 }
 
 CreateArray();
 PrintArray();
-PrintNumbers();
-PrintArray();
+PrintMinSum();
 
